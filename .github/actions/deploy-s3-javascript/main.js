0fd4757c8aa0327064cea94a60097608d3a292c3
dmytro.holovnia <dmytro.holovnia@gmail.com>
2024-09-30 08:27:32 +0300

    added js action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,14 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+    const bucketName = core.getInput('bucket', {require: true});
+    const bucketRegion = core.getInput('bucket-region', {require: true});
+    const distFolder = core.getInput('dist-folder', {require: true});
+
+    //2. File upload
+    const s3Uri = `s3//${bucketName}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
     core.notice('My test message')
 }
 
