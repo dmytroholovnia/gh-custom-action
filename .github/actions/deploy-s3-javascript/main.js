65e29ea3521e4860b5219772eea3169146c3f8a8
dmytro.holovnia <dmytro.holovnia@gmail.com>
2024-09-30 08:36:07 +0300

    fix uri

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,12 +3,13 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+    //1. Get inputs
     const bucketName = core.getInput('bucket', {require: true});
     const bucketRegion = core.getInput('bucket-region', {require: true});
     const distFolder = core.getInput('dist-folder', {require: true});
 
     //2. File upload
-    const s3Uri = `s3//${bucketName}`;
+    const s3Uri = `s3://${bucketName}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
     core.notice('My test message')
